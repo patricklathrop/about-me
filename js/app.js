2fb964101db8f795325ab412afd0773471fc84d4
patricklathrop <plathrop69@gmail.com>
2019-12-04 15:53:18 -0800

    update app.js to put line break in alert

--- js/app.js
+++ js/app.js
@@ -8,11 +8,11 @@ console.log('the javascript is linked correctly!');
 
 var username = prompt('Hello, what is your name!');
 //console.log('user\'s name was ' + username);
-alert('Welcome to my page ' + username + '.  Please use the paragraphs below to answer questions.');
+alert('Welcome to my page ' + username + '. \nPlease use the paragraphs below to answer questions.');
 
 function myQuiz() {
 
-var answer = prompt('I started making moonshine at the age of 12? y or n');
+var answer = prompt('I started making moonshine at the age of 12? y/yes or n/no');
 console.log('user\'s 1st answer was ' + answer);
 var lowerCaseAnswer = answer.toLowerCase();
 if ((lowerCaseAnswer === 'y') || (lowerCaseAnswer === 'yes')) {
@@ -21,7 +21,7 @@ if ((lowerCaseAnswer === 'y') || (lowerCaseAnswer === 'yes')) {
   alert('You chose incorrectly');
 }
 
-var answer = prompt('My brother taught me how to make moonshine? y or n');
+var answer = prompt('My brother taught me how to make moonshine? y/yes or n/no');
 console.log('user\'s 2nd answer was ' + answer);
 var lowerCaseAnswer = answer.toLowerCase();
 if ((lowerCaseAnswer === 'n') || (lowerCaseAnswer === 'no')) {
@@ -30,7 +30,7 @@ if ((lowerCaseAnswer === 'n') || (lowerCaseAnswer === 'no')) {
   alert('You chose incorrectly');
 }
 
-var answer = prompt('I joined the French Foreign Legion? y or n');
+var answer = prompt('I joined the French Foreign Legion? y/yes or n/no');
 console.log('user\'s 3rd answer was ' + answer);
 var lowerCaseAnswer = answer.toLowerCase();
 if ((lowerCaseAnswer === 'n') || (lowerCaseAnswer === 'no')) {
@@ -39,7 +39,7 @@ if ((lowerCaseAnswer === 'n') || (lowerCaseAnswer === 'no')) {
     alert('You chose incorrectly');
   }
 
-var answer = prompt('The study of cryptozoology includes bigfoot? y or n');
+var answer = prompt('The study of cryptozoology includes bigfoot? y/yes or n/no');
 console.log('user\'s 4th answer was ' + answer);
 var lowerCaseAnswer = answer.toLowerCase();
 if ((lowerCaseAnswer === 'y') || (lowerCaseAnswer === 'yes')) {
@@ -48,7 +48,7 @@ if ((lowerCaseAnswer === 'y') || (lowerCaseAnswer === 'yes')) {
     alert('You chose incorrectly');
   }
 
-var answer = prompt('The BFRO has their own website? y or n');
+var answer = prompt('The BFRO has their own website? y/yes or n/no');
 console.log('user\'s 5th answer was ' + answer);
 var lowerCaseAnswer = answer.toLowerCase();
 if ((lowerCaseAnswer === 'y') || (lowerCaseAnswer === 'yes')) {
