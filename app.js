daa4c337e2fe28615fb179adc08d535423d78830
patricklathrop <plathrop69@gmail.com>
2019-12-03 17:05:57 -0800

    add me.jpg and modify app.js, index.html style.css

--- app.js
+++ app.js
@@ -4,8 +4,12 @@
 // this console.log is just the test that we've hooked up the files correctly
 console.log('the javascript is linked correctly!');
 
+var username = prompt('Tell me your name!');
+//console.log('user\'s name was ' + username);
+alert('Welcome to my page ' + username + '.  Please use the paragraph below to answer 5 questions.');
+
 var answer = prompt('I started making moonshine at the age of 12? y or n');
-console.log('user\'s 1st answer was ' + answer);
+//console.log('user\'s 1st answer was ' + answer);
 var lowerCaseAnswer = answer.toLowerCase();
 if(lowerCaseAnswer === 'y') {
   alert('You chose correctly');
@@ -14,7 +18,7 @@ if(lowerCaseAnswer === 'y') {
 }
 
 var answer = prompt('My brother taught me how to make moonshine? y or n');
-console.log('user\'s 2nd answer was ' + answer);
+//console.log('user\'s 2nd answer was ' + answer);
 var lowerCaseAnswer = answer.toLowerCase();
 if(lowerCaseAnswer === 'n') {
   alert('You chose correctly');
@@ -23,7 +27,7 @@ if(lowerCaseAnswer === 'n') {
 }
 
 var answer = prompt('I joined the French Foreign Legion? y or n');
-console.log('user\'s 3rd answer was ' + answer);
+//console.log('user\'s 3rd answer was ' + answer);
 var lowerCaseAnswer = answer.toLowerCase();
 if(lowerCaseAnswer === 'n') {
     alert('You chose correctly');
@@ -32,7 +36,7 @@ if(lowerCaseAnswer === 'n') {
   }
 
 var answer = prompt('The study of cryptozoology includes bigfoot? y or n');
-console.log('user\'s 4th answer was ' + answer);
+//console.log('user\'s 4th answer was ' + answer);
 var lowerCaseAnswer = answer.toLowerCase();
 if(lowerCaseAnswer === 'y') {
     alert('You chose correctly');
@@ -41,21 +45,10 @@ if(lowerCaseAnswer === 'y') {
   }
 
 var answer = prompt('The BFRO has their own website? y or n');
-console.log('user\'s 5th answer was ' + answer);
+//console.log('user\'s 5th answer was ' + answer);
 var lowerCaseAnswer = answer.toLowerCase();
 if(lowerCaseAnswer === 'y') {
     alert('You chose correctly');
   } else {
     alert('You chose incorrectly');
   }
-
-// conditionals! let us use if, else if, else to run different pieces of code in different scenarios
-
-// if the username is Michelle, show a custom message
-// otherwise, for everyone else, show a generic message
-// var lowerCaseUsername = username.toLowerCase();
-// if(lowerCaseUsername === 'michelle') {
-//   alert('welcome back, you cool person ' + username);
-// } else {
-//   alert('Welcome to my site!');
-// }
