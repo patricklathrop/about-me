4a904c25e0cde4bab4c2ba3b9354ea5ac0babe77
patricklathrop <plathrop69@gmail.com>
2019-12-05 15:54:11 -0800

    local update

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # about-me
 An about me page created in week 1 Code 201
+
+lab 02/ day 2 - Web page initially created to ask visitor name, provide a couple paragraphs about myself (fictional) and provide a 5 question quiz that allows 'y/yes' or 'n/no' that are not case sensitive.  
+
+lab 03/ day 3 - Add a 'top 10' ordered list and work/education experience unordered list to the index.html file. / Add 6th question that is a multiple guess question (limit 4) for the user to pick a pre-chosen number. / Add 7th question that is multiple guess (limit 6) to select an answer that is define by an array. / Lastly, provide the user feedback on the number of correct/ incorrect questions answered.
+
+
--- js/app.js
+++ js/app.js
@@ -1,8 +1,6 @@
 'use strict';
 // use strict is almost alwayst the first line of our js files
 
-
-
 // this console.log is just the test that we've hooked up the files correctly
 console.log('the javascript is linked correctly!');
 
@@ -12,48 +10,88 @@ alert('Welcome to my page ' + username + '. \nPlease use the paragraphs below to
 
 function myQuiz() {
 
-var answer = prompt('I started making moonshine at the age of 12? y/yes or n/no');
-console.log('user\'s 1st answer was ' + answer);
-var lowerCaseAnswer = answer.toLowerCase();
-if ((lowerCaseAnswer === 'y') || (lowerCaseAnswer === 'yes')) {
-  alert('You chose correctly');
-} else {
-  alert('You chose incorrectly');
-}
+  var correct = 0;
+  var incorrect = 0;
 
-var answer = prompt('My brother taught me how to make moonshine? y/yes or n/no');
-console.log('user\'s 2nd answer was ' + answer);
-var lowerCaseAnswer = answer.toLowerCase();
-if ((lowerCaseAnswer === 'n') || (lowerCaseAnswer === 'no')) {
-  alert('You chose correctly');
-} else {
-  alert('You chose incorrectly');
-}
+  // console.log('Correct = ' + correct)
+  // console.log('Incorrect = ' + incorrect)
 
-var answer = prompt('I joined the French Foreign Legion? y/yes or n/no');
-console.log('user\'s 3rd answer was ' + answer);
-var lowerCaseAnswer = answer.toLowerCase();
-if ((lowerCaseAnswer === 'n') || (lowerCaseAnswer === 'no')) {
+  var answer = prompt('I started making moonshine at the age of 12? y/yes or n/no');
+  // console.log('user\'s 1st answer was ' + answer);
+  var lowerCaseAnswer1 = answer.toLowerCase();
+  if ((lowerCaseAnswer1 === 'y') || (lowerCaseAnswer1 === 'yes')) {
     alert('You chose correctly');
+    correct = correct + 1;
   } else {
     alert('You chose incorrectly');
+    incorrect = incorrect + 1;
   }
 
-var answer = prompt('The study of cryptozoology includes bigfoot? y/yes or n/no');
-console.log('user\'s 4th answer was ' + answer);
-var lowerCaseAnswer = answer.toLowerCase();
-if ((lowerCaseAnswer === 'y') || (lowerCaseAnswer === 'yes')) {
+  // console.log('Correct = ' + correct)
+  // console.log('Incorrect = ' + incorrect)
+
+  var answer = prompt('My brother taught me how to make moonshine? y/yes or n/no');
+  // console.log('user\'s 2nd answer was ' + answer);
+  var lowerCaseAnswer2 = answer.toLowerCase();
+  if ((lowerCaseAnswer2 === 'n') || (lowerCaseAnswer2 === 'no')) {
     alert('You chose correctly');
+    correct = correct + 1;
   } else {
     alert('You chose incorrectly');
+    incorrect = incorrect + 1;
   }
 
-var answer = prompt('The BFRO has their own website? y/yes or n/no');
-console.log('user\'s 5th answer was ' + answer);
-var lowerCaseAnswer = answer.toLowerCase();
-if ((lowerCaseAnswer === 'y') || (lowerCaseAnswer === 'yes')) {
+  // console.log('Correct = ' + correct)
+  // console.log('Incorrect = ' + incorrect)
+
+  var answer = prompt('I joined the French Foreign Legion? y/yes or n/no');
+  // console.log('user\'s 3rd answer was ' + answer);
+  var lowerCaseAnswer3 = answer.toLowerCase();
+  if ((lowerCaseAnswer3 === 'n') || (lowerCaseAnswer3 === 'no')) {
     alert('You chose correctly');
+    correct = correct + 1;
   } else {
     alert('You chose incorrectly');
+    incorrect = incorrect + 1;
   }
+
+  // console.log('Correct = ' + correct)
+  // console.log('Incorrect = ' + incorrect)
+
+  var answer = prompt('The study of cryptozoology includes bigfoot? y/yes or n/no');
+  // console.log('user\'s 4th answer was ' + answer);
+  var lowerCaseAnswer4 = answer.toLowerCase();
+  if ((lowerCaseAnswer4 === 'y') || (lowerCaseAnswer4 === 'yes')) {
+    alert('You chose correctly');
+    correct = correct + 1;
+  } else {
+    alert('You chose incorrectly');
+    incorrect = incorrect + 1;
+  }
+  // console.log('Correct = ' + correct)
+  // console.log('Incorrect = ' + incorrect)
+
+  var answer = prompt('The BFRO has their own website? y/yes or n/no');
+  // console.log('user\'s 5th answer was ' + answer);
+  var lowerCaseAnswer5 = answer.toLowerCase();
+  if ((lowerCaseAnswer5 === 'y') || (lowerCaseAnswer5 === 'yes')) {
+    alert('You chose correctly');
+    correct = correct + 1;
+  } else {
+    alert('You chose incorrectly');
+    incorrect = incorrect + 1;
+  }
+  // console.log('Correct = ' + correct)
+  // console.log('Incorrect = ' + incorrect)
 }
+
+// function picNumber() {
+
+//   var numRight = 5
+//   var i = 1
+
+//   var numGuess = prompt('Select a number between 1-10 - you have 4 chances' );
+// var numAnswer = Math.ceil(Math.random() *20);
+//   if (i < 5)
+//     else 
+
